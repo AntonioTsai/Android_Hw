71524993be91940e05fa19140908bb14f77c58c6
Antonio Tsai <jimmyjack7777@gmail.com>
2016-09-24 23:53:40 +0800

    Add feature Zoom In & Out

--- Lab1/app/src/main/java/antonio/lab1/Main2Activity.java
+++ Lab1/app/src/main/java/antonio/lab1/Main2Activity.java
@@ -3,12 +3,13 @@ package antonio.lab1;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
-
+    private TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,15 +17,23 @@ public class Main2Activity extends AppCompatActivity {
 
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        TextView string = (TextView) findViewById(R.id.demo);
+        text = (TextView) findViewById(R.id.demo);
         Button edit = (Button) findViewById(R.id.back);
 
-        string.setText(message);
+        text.setText(message);
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
             }
         });
+    }
+
+    public void zoomInText(View view) {
+        text.setTextSize(TypedValue.COMPLEX_UNIT_PX, text.getTextSize() + 1);
+    }
+
+    public void zoomOutText(View view) {
+        text.setTextSize(TypedValue.COMPLEX_UNIT_PX, text.getTextSize() - 1);
     }
 }
