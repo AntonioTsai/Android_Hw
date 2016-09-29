5eb7acb5d32f9cb537bcec9f93b7d674ce62960e
Antonio Tsai <jimmyjack7777@gmail.com>
2016-09-30 00:22:05 +0800

    Add feature change text color

--- Lab1/app/src/main/java/antonio/lab1/Main2Activity.java
+++ Lab1/app/src/main/java/antonio/lab1/Main2Activity.java
@@ -6,10 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
     private TextView text;
+    private int textColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +21,83 @@ public class Main2Activity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         text = (TextView) findViewById(R.id.demo);
         Button edit = (Button) findViewById(R.id.back);
+        final SeekBar seekBar_R = (SeekBar) findViewById(R.id.seekBar_R);
+        final SeekBar seekBar_G = (SeekBar) findViewById(R.id.seekBar_G);
+        final SeekBar seekBar_B = (SeekBar) findViewById(R.id.seekBar_B);
+        final TextView textView_color_R = (TextView) findViewById(R.id.textView_color_R);
+        final TextView textView_color_G = (TextView) findViewById(R.id.textView_color_G);
+        final TextView textView_color_B = (TextView) findViewById(R.id.textView_color_B);
+
+
 
         text.setText(message);
         edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+        seekBar_R.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView_color_R.setText("R " + progress + "/" + seekBar.getMax());
+                textColor = (seekBar_R.getProgress() << 16 & 0x00FF0000) +
+                        (seekBar_G.getProgress() << 8 & 0x0000FF00) +
+                        (seekBar_B.getProgress() & 0x000000FF) +
+                        0xFF000000;
+                text.setTextColor(textColor);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar_G.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView_color_G.setText("G " + progress + "/" + seekBar.getMax());
+                textColor = (seekBar_R.getProgress() << 16 & 0x00FF0000) +
+                        (seekBar_G.getProgress() << 8 & 0x0000FF00) +
+                        (seekBar_B.getProgress() & 0x000000FF) +
+                        0xFF000000;
+                text.setTextColor(textColor);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        seekBar_B.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView_color_B.setText("B " + progress + "/" + seekBar.getMax());
+                textColor = (seekBar_R.getProgress() << 16 & 0x00FF0000) +
+                        (seekBar_G.getProgress() << 8 & 0x0000FF00) +
+                        (seekBar_B.getProgress() & 0x000000FF) +
+                        0xFF000000;
+                text.setTextColor(textColor);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
