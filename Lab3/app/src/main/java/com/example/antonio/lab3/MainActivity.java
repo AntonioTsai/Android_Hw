9cf37269b6c1e4d34573549d0ed92fdb1b230cb2
Antonio Tsai <jimmyjack7777@gmail.com>
2016-11-05 23:17:25 +0800

    Add feature auto return from video

--- Lab3/app/src/main/java/com/example/antonio/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/antonio/lab3/MainActivity.java
@@ -39,6 +39,14 @@ public class MainActivity extends AppCompatActivity implements MediaPlayer.OnPre
         FileSelect = new Intent(Intent.ACTION_GET_CONTENT);
         FileSelect.setType("video/*");
         this.startActivityForResult(FileSelect, REQUEST_CODE);
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                FileSelect = new Intent(Intent.ACTION_GET_CONTENT);
+                FileSelect.setType("video/*");
+                startActivityForResult(FileSelect, REQUEST_CODE);
+            }
+        });
     }
 
     @Override
