5fabc2ed1e3fffc9ba83e64e0d92b767953734d5
Antonio Tsai <jimmyjack7777@gmail.com>
2016-11-23 19:26:06 +0800

    Set initial location at Tatung University

--- Lab5/app/src/main/java/com/example/antonio/lab5/MapsActivity.java
+++ Lab5/app/src/main/java/com/example/antonio/lab5/MapsActivity.java
@@ -1,8 +1,9 @@
 package com.example.antonio.lab5;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -39,8 +40,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // LatLng sydney = new LatLng(-34, 151);
+        LatLng tatung = new LatLng(25.067367, 121.521277);
+        CameraUpdate zoom = CameraUpdateFactory.zoomIn();
+
+        mMap.addMarker(new MarkerOptions().position(tatung).title("Marker in Sydney"));
+        // Method 1
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tatung, 16f));
+        // Method 2 With animation
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(tatung));
+        // mMap.animateCamera(CameraUpdateFactory.zoomTo(18f));
     }
 }
